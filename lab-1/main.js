103ce44fc3ff9362ce7b64453b59dfcc0dcf572d
matviizakrevskyi <matvii.zakrevskyi@microsoft.wsei.edu.pl>
2022-10-08 17:05:35 +0200

    lab-1 - zieew

--- lab-1/main.js
+++ lab-1/main.js
@@ -1,3 +1,4 @@
+//shame
 const liczba1 = document.querySelector('#liczba1')
 const liczba2 = document.querySelector('#liczba2')
 const liczba3 = document.querySelector('#liczba3')
@@ -22,4 +23,23 @@ btnPrzelicz.addEventListener('click', () => {
     + " min = " + Math.min.apply(null, liczby)
     + " max = " + Math.max.apply(null, liczby)
     )
+})
+
+
+//zieew
+const zwliczba1 = document.querySelector('#zw-liczba1')
+const zwliczba2 = document.querySelector('#zw-liczba2')
+const zwliczba3 = document.querySelector('#zw-liczba3')
+const zwliczba4 = document.querySelector('#zw-liczba4')
+const zwwynikiPojemnik = document.querySelector('#zw-wyniki')
+
+document.addEventListener('keyup', () => {
+    var zwliczby = [zwliczba1.value, zwliczba2.value, zwliczba3.value, zwliczba4.value]
+    
+    const zwsuma = zwliczby.reduce((a, b) => parseInt(a) + parseInt(b), 0)
+
+    zwwynikiPojemnik.innerHTML = "suma = " + zwsuma 
+    + " sriednia = " + zwsuma/4 
+    + " min = " + Math.min.apply(null, zwliczby)
+    + " max = " + Math.max.apply(null, zwliczby)
 })
