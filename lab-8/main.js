7cf98a8c00cf38b5820eca7eea96c05c969b3489
matviizakrevskyi <matvii.zakrevskyi@microsoft.wsei.edu.pl>
2022-12-12 03:51:54 +0100

    lab-8 - mouse attraction and repulsion

--- lab-8/main.js
+++ lab-8/main.js
@@ -1,8 +1,23 @@
 document.querySelector('.btn').addEventListener('click', Start)
+document.querySelector('#highload1').addEventListener('click', () => {
+    if(document.querySelector('#highload1').checked == true){
+        document.querySelector('#repulsion').disabled = false
+        document.querySelector('#attraction').disabled = false
+    }
+    else{
+        document.querySelector('#repulsion').disabled = true
+        document.querySelector('#attraction').disabled = true
+        document.querySelector('#repulsion').checked = false
+        document.querySelector('#attraction').checked = false
+    }
+})
 
 function Start(){
     document.querySelector('.btn').style.display = 'none';
     document.querySelector('.inputCount').style.display = 'none';
+    document.querySelector('.check').style.display = 'none';
+    document.querySelector('.radios').style.display = 'none';
+
     
     var canv = document.querySelector('.canv')
     var ctx = canv.getContext('2d')
@@ -11,7 +26,7 @@ function Start(){
     const distance = canv.width * (20/100)
     const count = document.querySelector('.inputCount').value
 
-    console.log(count)
+    console.log(distance)
     
     //Ball Class
     class Ball {
@@ -36,15 +51,23 @@ function Start(){
             ctx.closePath();
             ctx.fillStyle = 'black';
             ctx.fill();
+        }
+    }
 
-            ctx.beginPath();
-            ctx.arc(this.PosX, this.PosY, this.radius - 3, 0, Math.PI * 2, true);
-            ctx.closePath();
-            ctx.fillStyle = 'white';
-            ctx.fill();
+    //class vector
+    class Vector {
+        x
+        y
+        length
+
+        constructor(x1, y1, x2, y2){
+            this.x = x2 - x1
+            this.y = y2 - y1
+            this.length = (this.x**2 + this.y**2)**(1/2)
         }
     }
     
+    //animation
     function animationBall(balls) {
         ctx.clearRect(0, 0, canv.width, canv.height)
 
@@ -55,7 +78,9 @@ function Start(){
             //lines
             for (let j = 0; j < balls.length; j++){
                 var c = ((balls[i].PosX - balls[j].PosX)**2 
-                + (balls[i].PosY - balls[j].PosY)**2)**(1/2)
+                    + (balls[i].PosY - balls[j].PosY)**2)**(1/2)
+                
+                
                 
                 if (c < distance && c > 0){
                     ctx.beginPath()
@@ -75,6 +100,30 @@ function Start(){
             ctx.closePath();
             ctx.fillStyle = 'white';
             ctx.fill();
+            
+            //mouse
+            if(document.querySelector('#highload1').checked == true){
+                var c = ((balls[i].PosX - mouseX)**2 
+                + (balls[i].PosY - mouseY)**2)**(1/2)
+                
+                if (c < distance / 2 && c > 0){
+                    var CursorVct = new Vector(mouseX, mouseY, balls[i].PosX, balls[i].PosY)
+                    var lengthViceVct = distance - CursorVct.length
+                    var ResX = CursorVct.x * lengthViceVct / CursorVct.length
+                    var ResY = CursorVct.y * lengthViceVct / CursorVct.length
+                    
+                    if(document.querySelector('#repulsion').checked == true){
+                        balls[i].VX = ResX / 50
+                        balls[i].VY = ResY / 50
+                    }
+                    if(document.querySelector('#attraction').checked == true){
+                        balls[i].VX = - ResX / 50
+                        balls[i].VY = - ResY / 50
+                    }
+                }
+            }
+            //
+
 
             if (balls[i].PosX + balls[i].VX  > canv.width || balls[i].PosX + balls[i].VX < 0){
                 balls[i].VX = -balls[i].VX
@@ -84,15 +133,23 @@ function Start(){
             }
             balls[i].PosX += balls[i].VX
             balls[i].PosY += balls[i].VY
+
+            if(idBallEs == balls[i].id){
+                balls[i].PosX = mouseX
+                balls[i].PosY = mouseY
+            }
         }
+
+        
 
         requestAnimationFrame(function(){animationBall(balls)})
     }
 
+    //random func
     function getRandom(min, max) {
         return Math.random() * (max - min) + min;
     }
-    //
+    
 
     //start
     var balls = new Array()
@@ -108,7 +165,38 @@ function Start(){
     requestAnimationFrame(function(){animationBall(balls)})
 
     
-    
+    //mouse
+    canv.addEventListener('mousemove', onMouseMove)
 
+    var mouseX
+    var mouseY
+    
+    function onMouseMove(event){
+        mouseX = event.x - 27   //-27
+        mouseY = event.y - 19   //-19
+    }
+
+    var idBallEs = null
+    var isClick = false
+    
+    document.addEventListener('click', () => {
+        if(isClick == false || idBallEs == null){
+            for(let i = 0; i < balls.length; i++){
+                if(mouseX < balls[i].PosX + 10 &&
+                    mouseX > balls[i].PosX - 10 && 
+                    mouseY < balls[i].PosY + 10 &&
+                    mouseY > balls[i].PosY - 10
+                    ){
+                        idBallEs = balls[i].id
+                }
+            }
+            isClick = true
+        }
+        else if(isClick == true){
+            idBallEs = null
+            isClick = false
+        }
+
+    })
 
 }
